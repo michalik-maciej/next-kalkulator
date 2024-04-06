d0dec71dd35193ccf42d4712404793d6feddd8a8
Maciej Michalik <halniak24@gmail.com>
2024-04-07 00:19:36 +0200

    add shelves

--- app/form/Gondola.tsx
+++ app/form/Gondola.tsx
@@ -3,13 +3,13 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { indexOf, size } from 'lodash/fp'
 import {
-  ArrowLeftToLine,
-  ArrowRightFromLine,
   BetweenVerticalEnd,
+  FoldHorizontal,
   MoveLeft,
   MoveRight,
   Plus,
   Trash2,
+  UnfoldHorizontal,
 } from 'lucide-react'
 import { useFieldArray, useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -31,9 +31,18 @@ import {
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 import { toast } from '@/components/ui/use-toast'
 
-const FormSchema = z.object({
+import { Shelves } from './Shelves'
+
+export const FormSchema = z.object({
   foot: z.enum(['37', '47', '57']),
-  stands: z.array(z.object({ width: z.enum(['80', '100', '120']) })),
+  stands: z.array(
+    z.object({
+      width: z.enum(['80', '100', '120']),
+      shelves: z.array(
+        z.object({ amount: z.number(), depth: z.enum(['37', '47', '57']) }),
+      ),
+    }),
+  ),
 })
 
 export function Gondola() {
@@ -41,10 +50,13 @@ export function Gondola() {
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
-    defaultValues: { foot: '47', stands: [{ width: '100' }] },
+    defaultValues: {
+      foot: '47',
+      stands: [{ width: '100', shelves: [{ amount: 3, depth: '47' }] }],
+    },
   })
 
-  const { append, fields, insert, remove, swap } = useFieldArray({
+  const stands = useFieldArray({
     control: form.control,
     name: 'stands',
   })
@@ -99,100 +111,124 @@ export function Gondola() {
             )
           }}
         />
-        <div
-          className="flex border-2 my-32"
-          style={{ height: `${2 * Number(form.getValues('foot'))}px` }}>
-          {fields.map((stand, index) => (
-            <FormField
-              key={stand.id}
-              control={form.control}
-              name={`stands.${index}.width`}
-              render={({ field }) => {
-                const currentValueIndex = indexOf(field.value, widthOptions)
+        <div className="flex my-16">
+          {stands.fields.map((stand, index) => {
+            const foot = form.getValues('foot')
+            const width = form.getValues(`stands.${index}.width`)
+            const shelves = form
+              .getValues(`stands.${index}.shelves`)
+              .map(({ amount, depth }) => `${amount}x${depth}`)
 
-                return (
-                  <Popover>
-                    <PopoverTrigger>
-                      <div
-                        style={{ width: `${2 * Number(field.value)}px` }}
-                        className="p-4 border-2 transition-width h-full">
-                        <div className="my-auto">
-                          {[field.value, form.getValues('foot')].join('/')}
-                        </div>
-                      </div>
-                    </PopoverTrigger>
-                    <PopoverContent>
-                      <FormItem>
-                        <div className="flex">
-                          <Button
-                            variant="ghost"
-                            disabled={index < 1}
-                            size="icon"
-                            onClick={() => swap(index, index - 1)}>
-                            <MoveLeft />
-                          </Button>
-                          <Button
-                            variant="ghost"
-                            size="icon"
-                            onClick={() =>
-                              insert(index + 1, { width: field.value })
-                            }>
-                            <BetweenVerticalEnd />
-                          </Button>
-                          <Button
-                            size="icon"
-                            variant="ghost"
-                            disabled={currentValueIndex < 1}
-                            onClick={() =>
-                              form.setValue(
-                                field.name,
-                                widthOptions[currentValueIndex - 1],
-                              )
-                            }>
-                            <ArrowLeftToLine />
-                          </Button>
-                          <Button
-                            size="icon"
-                            variant="ghost"
-                            disabled={
-                              currentValueIndex >= size(widthOptions) - 1
-                            }
-                            onClick={() =>
-                              form.setValue(
-                                field.name,
-                                widthOptions[currentValueIndex + 1],
-                              )
-                            }>
-                            <ArrowRightFromLine />
-                          </Button>
-                          <Button
-                            variant="ghost"
-                            size="icon"
-                            onClick={() => remove(index)}>
-                            <Trash2 />
-                          </Button>
-                          <Button
-                            variant="ghost"
-                            disabled={index >= size(fields) - 1}
-                            size="icon"
-                            onClick={() => swap(index, index + 1)}>
-                            <MoveRight />
-                          </Button>
-                        </div>
-                        <FormMessage />
-                      </FormItem>
-                    </PopoverContent>
-                  </Popover>
-                )
-              }}
-            />
-          ))}
-          {!size(fields) && (
+            return (
+              <Popover key={stand.id}>
+                <PopoverTrigger>
+                  <div
+                    style={{
+                      height: `${2 * Number(foot)}px`,
+                      width: `${2 * Number(width)}px`,
+                    }}
+                    className="p-4 border-2 transition-width h-full">
+                    <div>{[width, foot].join('/')}</div>
+                    <div>{shelves.join(' + ')}</div>
+                  </div>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <div>
+                    stand
+                    <Button
+                      variant="ghost"
+                      disabled={index < 1}
+                      size="icon"
+                      onClick={() => stands.swap(index, index - 1)}>
+                      <MoveLeft />
+                    </Button>
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      onClick={() =>
+                        stands.insert(index + 1, {
+                          width,
+                          shelves: [{ amount: 3, depth: '37' }],
+                        })
+                      }>
+                      <BetweenVerticalEnd />
+                    </Button>
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      onClick={() => stands.remove(index)}>
+                      <Trash2 />
+                    </Button>
+                    <Button
+                      variant="ghost"
+                      disabled={index >= size(stands.fields) - 1}
+                      size="icon"
+                      onClick={() => stands.swap(index, index + 1)}>
+                      <MoveRight />
+                    </Button>
+                  </div>
+                  <FormField
+                    control={form.control}
+                    name={`stands.${index}.width`}
+                    render={({ field }) => {
+                      const currentWidthIndex = indexOf(
+                        field.value,
+                        widthOptions,
+                      )
+
+                      return (
+                        <FormItem>
+                          <div className="flex">
+                            width
+                            <Button
+                              size="icon"
+                              variant="ghost"
+                              disabled={currentWidthIndex < 1}
+                              onClick={() => {
+                                form.setValue(
+                                  field.name,
+                                  widthOptions[currentWidthIndex - 1],
+                                )
+                                form.trigger(field.name)
+                              }}>
+                              <FoldHorizontal />
+                            </Button>
+                            <Button
+                              size="icon"
+                              variant="ghost"
+                              disabled={
+                                currentWidthIndex >= size(widthOptions) - 1
+                              }
+                              onClick={() => {
+                                form.setValue(
+                                  field.name,
+                                  widthOptions[currentWidthIndex + 1],
+                                )
+                                form.trigger(field.name)
+                              }}>
+                              <UnfoldHorizontal />
+                            </Button>
+                          </div>
+                        </FormItem>
+                      )
+                    }}
+                  />
+                  <Shelves standIndex={index} />
+                </PopoverContent>
+              </Popover>
+            )
+          })}
+          {!size(stands.fields) && (
             <Button
               className="my-auto mx-8"
               variant="outline"
               size="icon"
-              onClick={() => append({ width: '100' })}>
+              onClick={() =>
+                stands.append({
+                  width: '100',
+                  shelves: [{ amount: 3, depth: '37' }],
+                })
+              }>
               <Plus />
             </Button>
           )}
--- app/form/Shelves.tsx
+++ app/form/Shelves.tsx
@@ -1,38 +1,124 @@
 'use client'
 
-import { Product } from '@prisma/client'
-import { filter } from 'lodash/fp'
-import { Plus } from 'lucide-react'
+import { indexOf, size } from 'lodash/fp'
+import {
+  ChevronDown,
+  ChevronUp,
+  FoldVertical,
+  Plus,
+  Trash2,
+  UnfoldVertical,
+} from 'lucide-react'
 import { useFieldArray, useFormContext } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
+import { FormField, FormItem } from '@/components/ui/form'
+
+import { FormSchema } from './Gondola'
 
 interface Props {
-  products: Product[]
+  standIndex: number
 }
 
-export const Shelves = ({ products }: Props) => {
-  const { control, getValues } = useFormContext()
-  const options = filter(
-    { category: 'shelf', width: Number(getValues().width) },
-    products,
-  )
-  const {
-    fields: shelves,
-    append,
-    remove,
-  } = useFieldArray({
-    control,
-    name: 'shelves',
+export const Shelves = ({ standIndex }: Props) => {
+  const depthOptions = ['37', '47', '57'] as const
+
+  const form = useFormContext<z.infer<typeof FormSchema>>()
+
+  const shelves = useFieldArray({
+    control: form.control,
+    name: `stands.${standIndex}.shelves`,
   })
 
   return (
-    <div className="flex rounded-lg gap-2 flex-col border-2 m-8 p-4 w-1/3 items-center">
-      Półki
-      {shelves.map((field, index) => {
-        return <></>
-      })}
-      <Button variant="outline" size="icon" onClick={append}>
+    <div className="flex rounded-lg gap-2 flex-col border-2 m-8 p-4 items-center">
+      półki
+      {shelves.fields.map((field, index) => (
+        <div key={field.id}>
+          <FormField
+            control={form.control}
+            name={`stands.${standIndex}.shelves.${index}.amount`}
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <div className="flex">
+                    <Button
+                      size="icon"
+                      disabled={field.value <= 1}
+                      variant="ghost"
+                      onClick={() => {
+                        form.setValue(field.name, field.value - 1)
+                        form.trigger(field.name)
+                      }}>
+                      <ChevronDown />
+                    </Button>
+                    <Button
+                      size="icon"
+                      variant="ghost"
+                      onClick={() => {
+                        form.setValue(field.name, field.value + 1)
+                        form.trigger(field.name)
+                      }}>
+                      <ChevronUp />
+                    </Button>
+                  </div>
+                </FormItem>
+              )
+            }}
+          />
+          <FormField
+            control={form.control}
+            name={`stands.${standIndex}.shelves.${index}.depth`}
+            render={({ field }) => {
+              const currentDepthIndex = indexOf(field.value, depthOptions)
+
+              return (
+                <FormItem>
+                  <div className="flex">
+                    <Button
+                      size="icon"
+                      variant="ghost"
+                      disabled={currentDepthIndex < 1}
+                      onClick={() => {
+                        form.setValue(
+                          field.name,
+                          depthOptions[currentDepthIndex - 1],
+                        )
+                        form.trigger(field.name)
+                      }}>
+                      <FoldVertical />
+                    </Button>
+                    <Button
+                      size="icon"
+                      variant="ghost"
+                      disabled={currentDepthIndex >= size(depthOptions) - 1}
+                      onClick={() => {
+                        form.setValue(
+                          field.name,
+                          depthOptions[currentDepthIndex + 1],
+                        )
+                        form.trigger(field.name)
+                      }}>
+                      <UnfoldVertical />
+                    </Button>
+                  </div>
+                </FormItem>
+              )
+            }}
+          />
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => shelves.remove(index)}>
+            <Trash2 />
+          </Button>
+        </div>
+      ))}
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={() => shelves.append({ amount: 3, depth: '37' })}>
         <Plus />
       </Button>
     </div>
